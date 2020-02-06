dd9faf2f3408a30dc16c258e5cc1e8b8e1d4b00d
Pedrodebdx <pv.ferrat@gmail.com>
2020-02-06 16:16:09 +0100

    16H16

--- robot.py
+++ robot.py
@@ -1,14 +1,25 @@
 class MonRobot:
     #class des robots
     robots_crees = 0
-    def __init__(self, c_nom='sans nom', position="0,0", c_direction='0'):
+    def __init__(self, c_nom='sans nom', position_x="0", position_y="0", c_direction='0'):
         self.nom = c_nom
-        self.position = position
+        self.position_x = position_x
+        self.position_y = position_y
         MonRobot.robots_crees +=1
         self.direction = c_direction
 
+
+    def avancer(self, avance):
+        if avance == "Est" :
+            self.position_x += 1
+        if avance == "Nord":
+            self.position_y += 1
+
+    def tourner(self, tourne):
+        print('tourne à gauche marcel')
+
 print("Création d'un robot !")
-robot1 = MonRobot('nono le robot', '43 , 12')
-print(f'Nom du robot 1: {robot2.nom }')
-print(f'Sa postion est : {robot2.position }')
+robot1 = MonRobot('nono le robot', '43' , '12')
+print(f'Nom du robot 1: {robot1.nom }')
+print(f'Sa postion est : {robot1.position_x}, {robot1.position_y}')
 print(f'Nombres de robots crées :{MonRobot.robots_crees}')
